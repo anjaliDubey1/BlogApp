5454c7820a883bebd1ec8ae3c43600a6b164d615
Anjali Dubey <andubey@bestpeers.com>
2022-06-27 11:00:40 +0530

    added index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+
+  get "/welcome", to: "articles#index"
+  get "/welcome/:id", to: "articles#show"
 end
